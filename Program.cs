7f5d2e835d7a265b92b387643d82c7afd2540e87
Jenny Pichardo <mlpichardo1@gmail.com>
2018-05-12 12:22:13 -0400

    visual studio setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectOrientedProgramminginC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
