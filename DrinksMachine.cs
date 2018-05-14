6e12cff3049869634d6400e38e6852bc7ab0d50b
Jenny Pichardo <mlpichardo1@gmail.com>
2018-05-14 18:01:58 -0400

    lesson 1

---
+++ DrinksMachine.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ObjectOrientedProgramminginC_
+{
+public class DrinksMachine
+{
+   // private member variables
+   private int age;
+   private string make;
+
+   // public properties
+   public int Age
+   {
+      get
+      {
+         return age;
+      }
+      set
+      {
+         age = value;
+      }
+   }
+   public string Make
+   {
+      get
+      {
+ 
+      }
+   }
+   // Auto-implemented properties 
+public double TotalPurchases { get; set; }
+public string Name { get; set; }
+public int CustomerID { get; set; }
+}
+}
